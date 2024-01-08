0b728555f67a76d9cdeb428faf38cecef8434ee1
souha gharsallah <souha.gharsallah@polytechnicien.tn>
2024-01-08 17:34:26 +0100

    ajouter

---
+++ square.py
@@ -0,0 +1,13 @@
+import math
+t=int(input())
+for i in range(t):
+    s=0
+    n=int(input())
+    a= list(map(int, input().split()))
+    for i in range(n):
+        s=s+a[i]
+    m=math.sqrt(s)
+    if int (m) * int (m)==s:
+        print("YES")
+    else:
+        print("NO")    
